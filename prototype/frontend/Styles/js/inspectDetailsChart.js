1184fe9fdd1bffc7cb5d4e8ebd01aacf25bdb95c
Sofiane ELGUENDOUZE <fs_elguendouze@esi.dz>
2019-09-28 13:24:40 +0900

    add java script chart variable and informations

--- prototype/frontend/Styles/js/inspectDetailsChart.js
+++ prototype/frontend/Styles/js/inspectDetailsChart.js
@@ -1,1 +1,31 @@
-
+var myBarChart = new Chart(document.getElementById("bar-chart-grouped"), {
+    type: 'bar',
+    data: {
+      labels: ["Repository 1", "Repository 2", "Repository 3", "Repository 4"],
+      datasets: [
+        {
+          label: "Naming Conventions",
+          backgroundColor: "#3e95cd",
+          data: [95, 80, 98, 70]
+        }, {
+          label: "Documentation",
+          backgroundColor: "#8e5ea2",
+          data: [20, 30, 35, 15]
+        }, {
+            label: "Format Conventions",
+            backgroundColor: "#26a69a",
+            data: [50, 44, 60, 90]
+        }, {
+            label: "Code Consistency",
+            backgroundColor: "#f57f17",
+            data: [60, 23, 78, 60]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Population growth (millions)'
+      }
+    }
+});
