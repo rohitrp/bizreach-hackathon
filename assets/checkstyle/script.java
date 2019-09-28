83bbf769ce64a054875d513fdb21213191c52a43
minhtuan2000 <minhtuan270820000@gmail.com>
2019-09-28 03:20:00 +0000

    finished the code maintainability apis

--- assets/checkstyle/script.java
+++ assets/checkstyle/script.java
@@ -1,61 +1,125 @@
-/**
- * The MIT License
- * Copyright (c) 2014 Ilkka Seppälä
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-/**
- * 
- */
-package com.iluwatar.throttling.timer;
+package computing.mobile.ambulance;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.iluwatar.throttling.CallsCount;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 
-/**
- * Implementation of throttler interface. This class resets the counter every second.
- * @author drastogi
- *
- */
-public class ThrottleTimerImpl implements Throttler {
+import org.json.JSONException;
+import org.json.JSONObject;
+import computing.mobile.ambulance.util.Constant;
+import computing.mobile.ambulance.util.ServerRequest;
 
-  private final int throttlePeriod;
-  private final CallsCount callsCount;
+import static computing.mobile.ambulance.R.id.btnsendgps;
+import static computing.mobile.ambulance.R.id.submenuarrow;
 
-  public ThrottleTimerImpl(int throttlePeriod, CallsCount callsCount) {
-    this.throttlePeriod = throttlePeriod;
-    this.callsCount = callsCount;
-  }
-  
-  /**
-   * A timer is initiated with this method. The timer runs every second and resets the
-   * counter.
-   */
-  @Override
-  public void start() {
-    new Timer(true).schedule(new TimerTask() {
-      @Override
-      public void run() {
-        callsCount.reset();
-      }
-    }, 0, throttlePeriod);
-  }
+public class MainActivity extends AppCompatActivity {
+    public static Context ambulance_context;
+    Button sendLocation;
+    public static TextView textview;
+
+
+
+    private void sendGPStoServer(String lastLocationValue) {
+        String url = Constant.url + "/ambulance/gps";
+
+        JSONObject param = new JSONObject();
+        try {
+            param.put("UserID", "");
+            param.put("GPS", lastLocationValue);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest dpUpload = new JsonObjectRequest(Request.Method.POST, url, param,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+
+        ServerRequest.getInstance(MainActivity.this).getRequestQueue().add(dpUpload);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        ambulance_context = MainActivity.this;
+        setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
+        }
+
+        else{
+            Toast.makeText(MainActivity.this
+            ,"Permission OK",Toast.LENGTH_SHORT).show();
+        }
+
+        textview = (TextView)findViewById(R.id.textViewgps);
+        Intent gpsintent = new Intent(MainActivity.this,GPS.class);
+        startService(gpsintent);
+        textview.setText("Location of Ambulance is :"+"Loading ....");
+
+
+        sendLocation =(Button)findViewById(R.id.btnsendgps);
+        sendLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendGPStoServer(GPS.lastLocationValue);
+
+            }
+        });
+    }
+
+
+    public void onDestroy() {
+        stopService(new Intent(MainActivity.this, GPS.class));
+        super.onDestroy();
+    }
+
+    public void onPause(){
+        super.onPause();
+        stopService(new Intent(MainActivity.this, GPS.class));
+    }
+
+    public void onResume(){
+        super.onResume();
+        startService(new Intent(MainActivity.this, GPS.class));
+    }
 }
