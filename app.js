342f641c9f2b34ef81ca7f79a184474cf018d29f
minhtuan2000 <minhtuan270820000@gmail.com>
2019-09-28 01:48:44 +0000

    written some code

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api', require('./routes/repos'));
 app.use('/api', require('./routes/users'));
+app.use('/api', require('./routes/code'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
